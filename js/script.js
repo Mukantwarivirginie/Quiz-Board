491ea0589f3dab4945c5aa871413aa7391591cb3
Wecode <Mukantwarivirginie>
2018-12-21 17:23:50 +0200

    added readme

--- js/script.js
+++ js/script.js
@@ -1,25 +1,30 @@
 $(document).ready(function() {
-    $("#blanks form").submit(function() {
+    $("#blanks").submit(function(event) {
+
     var Question1 = $("input[name='Qestion1']:checked").val();    
     var Question2 = $("input[name='Qestion2']:checked").val();
     var Question3 = $("input[name='Qestion3']:checked").val();
     var Question4 = $("input[name='Qestion4']:checked").val();
-    var result= 0;
+
+    var total= 0;
+
     if (Question1===15){
-        result+=15;
+        total+=15;
     }
     if (Question2===15){
-        result+=15;
+        total+=15;
     }
       if (Question3===15){
-          result+=15;
+          total+=15;
       } 
      if (Question4===15){
-         result+=15;
+         total+=15;
      }
-         $(".final").show();
-         $("display").text("your scole is:"+result)
+    $(".final").show();
+    $("#display").text("Your final result: " + total)
+     event.preventDefault();  
     });
+    
 });
 
 
