faf75bca258653c3f7f5cbf83c951aacb306e4ed
Wecode <Mukantwarivirginie>
2019-01-07 13:13:13 +0200

    correct the file linking

--- js/script.js
+++ js/script.js
@@ -1,31 +1,44 @@
 $(document).ready(function() {
-    $("#blanks").submit(function(event) {
+    $("input[type='button']").click(function(event) {
 
-    var Question1 = $("input[name='Qestion1']:checked").val();    
-    var Question2 = $("input[name='Qestion2']:checked").val();
-    var Question3 = $("input[name='Qestion3']:checked").val();
-    var Question4 = $("input[name='Qestion4']:checked").val();
+    var Question1 = $("input[name='Q1']:checked").val();    
+    var Question2 = $("input[name='Q2']:checked").val();
+    var Question3 = $("input[name='Q3']:checked").val();
+    var Question4 = $("input[name='Q4']:checked").val();
 
     var total= 0;
 
-    if (Question1===15){
+    if (Question1==="C"){
         total+=15;
     }
-    if (Question2===15){
+    else {
+        total+=0;
+    }
+    if (Question2=== "C"){
         total+=15;
     }
-      if (Question3===15){
+    else{
+        total+=0
+    }
+      if (Question3=== "C"){
           total+=15;
       } 
-     if (Question4===15){
+      else{
+          total+=0;
+      }
+     if (Question4=== "C"){
          total+=15;
      }
-    $(".final").show();
-    $("#display").text("Your final result: " + total)
+     else total+=0;
+     
+    
+   $(".score").text("your score is " + total);
      event.preventDefault();  
     });
     
 });
+
+
 
 
 
